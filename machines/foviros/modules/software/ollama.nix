2946e40e4e54b9ad30c0ce07c179e83e93ebaa89
Fovir <fovir@disroot.org>
2026-02-26 16:50:55 +0800

    feat: use `ollama-cuda`

--- machines/foviros/modules/software/ollama.nix
+++ machines/foviros/modules/software/ollama.nix
@@ -1,8 +1,27 @@
-{...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
+  nixpkgs.config.allowUnfreePredicate = pkg:
+    builtins.elem (lib.getName pkg) [
+      "cuda_cccl"
+      "cuda_cudart"
+      "cuda_nvcc"
+      "libcublas"
+      "nvidia-settings"
+      "nvidia-x11"
+    ];
+
   services.ollama = {
     enable = true;
 
-    loadModels = ["llama3.1:70b"];
+    package = pkgs.ollama-cuda;
+    loadModels = [
+      "gemma3:12b"
+      "gpt-oss:20b"
+      "llama3.2:3b"
+    ];
     syncModels = true;
   };
 }

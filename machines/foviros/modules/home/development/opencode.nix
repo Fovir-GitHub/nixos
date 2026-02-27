37bc9f0e8eb88f24e017298e70502d8d253e2891
Fovir <fovir@disroot.org>
2026-02-27 23:34:46 +0800

    feat: add ollama local provider

--- machines/foviros/modules/home/development/opencode.nix
+++ machines/foviros/modules/home/development/opencode.nix
@@ -1,5 +1,32 @@
 {...}: {
   programs.opencode = {
     enable = true;
+
+    settings = {
+      autoupdate = true;
+      theme = "opencode";
+      provider = {
+        ollama = {
+          models = let
+            mkModel = name: {
+              name = name;
+              value = {
+                inherit name;
+              };
+            };
+          in
+            builtins.listToAttrs (map mkModel [
+              "gpt-oss:20b"
+              "qwen3-coder:30b"
+              "qwen3-vl:30b"
+            ]);
+          name = "Ollama (local)";
+          npm = "@ai-sdk/openai-compatible";
+          options = {
+            baseURL = "http://127.0.0.1:11434/v1";
+          };
+        };
+      };
+    };
   };
 }

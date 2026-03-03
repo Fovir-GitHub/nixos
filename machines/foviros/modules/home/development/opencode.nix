1d856403dd6326587c6580da06e3d785d55b3430
Fovir <fovir@disroot.org>
2026-03-03 14:12:38 +0800

    feat: add `qwen2.5-coder:32b-instruct-q3_K_S`

--- machines/foviros/modules/home/development/opencode.nix
+++ machines/foviros/modules/home/development/opencode.nix
@@ -17,6 +17,7 @@
           in
             builtins.listToAttrs (map mkModel [
               "gpt-oss:20b"
+              "qwen2.5-coder:32b-instruct-q3_K_S"
               "qwen2.5-coder:32b-instruct-q4_0"
               "qwen3-coder:30b"
               "qwen3-coder:30b-a3b-q4_K_M"
--- machines/foviros/modules/software/ollama.nix
+++ machines/foviros/modules/software/ollama.nix
@@ -26,6 +26,7 @@
       "llama3.2-vision:11b"
       "llama3.2:3b"
       "qwen2.5-coder:14b"
+      "qwen2.5-coder:32b-instruct-q3_K_S"
       "qwen2.5-coder:32b-instruct-q4_0"
       "qwen3-coder:30b"
       "qwen3-coder:30b-a3b-q4_K_M"

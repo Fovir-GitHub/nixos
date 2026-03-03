6a02770c36a026093d5458dc26438554bfb58d3a
Fovir <fovir@disroot.org>
2026-03-03 23:34:11 +0800

    chore: remove overlay of `hlchunk-nvim`

--- modules/shared/overlays.nix
+++ modules/shared/overlays.nix
@@ -1,27 +1,9 @@
-{
-  inputs,
-  pkgs,
-  ...
-}: {
+{inputs, ...}: {
   nixpkgs.overlays = [
     (_final: prev: {
       sui = prev.callPackage ../../pkgs/sui {
         platform = prev.stdenv.hostPlatform;
       };
-    })
-    (final: prev: {
-      vimPlugins =
-        prev.vimPlugins
-        // {
-          hlchunk-nvim = prev.vimPlugins.hlchunk-nvim.overrideAttrs (oldAttrs: {
-            src = pkgs.fetchFromGitHub {
-              owner = "shellRaining";
-              repo = "hlchunk.nvim";
-              rev = "3bc2bd7aef28fbed6643534a0fdd0f19966544bc";
-              sha256 = "sha256-NcfH9AnURII+MTSoyDgnotpzjcLNxXx1BXnKYXP2C/0=";
-            };
-          });
-        };
     })
     inputs.nix-vscode-extensions.overlays.default
   ];

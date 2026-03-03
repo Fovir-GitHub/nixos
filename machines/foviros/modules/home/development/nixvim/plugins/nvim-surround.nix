c67f42586724c6bd7ce509eae872d0548583fb5b
Fovir <fovir@disroot.org>
2026-03-03 23:30:02 +0800

    fix: migrate from v3 to v4

--- machines/foviros/modules/home/development/nixvim/plugins/nvim-surround.nix
+++ machines/foviros/modules/home/development/nixvim/plugins/nvim-surround.nix
@@ -2,47 +2,52 @@
   plugins.nvim-surround = {
     enable = true;
 
-    settings = {
-      keymaps.visual = "S";
-
-      surrounds = {
-        "(" = {
-          add = ["(" ")"];
-          delete = "^(. ?)().-( ?.)()$";
-          find.__raw = ''
-            function()
-              return M.get_selection({ motion = "a(" })
-            end
-          '';
-        };
-        "[" = {
-          add = ["[" "]"];
-          delete = "^(. ?)().-( ?.)()$";
-          find.__raw = ''
-            function()
-              return M.get_selection({ motion = "a[" })
-            end
-          '';
-        };
-        "{" = {
-          add = ["{" "}"];
-          delete = "^(. ?)().-( ?.)()$";
-          find.__raw = ''
-            function()
-              return M.get_selection({ motion = "a{" })
-            end
-          '';
-        };
-        "<" = {
-          add = ["<" ">"];
-          delete = "^(. ?)().-( ?.)()$";
-          find.__raw = ''
-            function()
-              return M.get_selection({ motion = "a<" })
-            end
-          '';
-        };
+    settings.surrounds = {
+      "(" = {
+        add = ["(" ")"];
+        delete = "^(. ?)().-( ?.)()$";
+        find.__raw = ''
+          function()
+            return M.get_selection({ motion = "a(" })
+          end
+        '';
+      };
+      "[" = {
+        add = ["[" "]"];
+        delete = "^(. ?)().-( ?.)()$";
+        find.__raw = ''
+          function()
+            return M.get_selection({ motion = "a[" })
+          end
+        '';
+      };
+      "{" = {
+        add = ["{" "}"];
+        delete = "^(. ?)().-( ?.)()$";
+        find.__raw = ''
+          function()
+            return M.get_selection({ motion = "a{" })
+          end
+        '';
+      };
+      "<" = {
+        add = ["<" ">"];
+        delete = "^(. ?)().-( ?.)()$";
+        find.__raw = ''
+          function()
+            return M.get_selection({ motion = "a<" })
+          end
+        '';
       };
     };
   };
+
+  keymaps = [
+    {
+      action = "<Plug>(nvim-surround-visual)";
+      key = "S";
+      mode = ["x"];
+      options.desc = "[S]urround";
+    }
+  ];
 }

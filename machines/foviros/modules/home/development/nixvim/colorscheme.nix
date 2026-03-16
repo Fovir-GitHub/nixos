773e904193fc1b5aef50df19f22ce3788aafbb97
Fovir <fovir@disroot.org>
2026-03-17 00:33:59 +0800

    feat: use `tokyonight` theme

--- machines/foviros/modules/home/development/nixvim/colorscheme.nix
+++ machines/foviros/modules/home/development/nixvim/colorscheme.nix
@@ -1,18 +1,38 @@
 {...}: {
-  colorschemes.catppuccin = {
-    enable = true;
-    settings = {
-      flavour = "mocha";
-      transparent_background = false;
-      term_colors = true;
-      default_integrations = true;
-      integrations = {
-        cmp = true;
-        neotree = true;
-        telescope.enabled = true;
-        dashboard = true;
+  colorschemes = {
+    catppuccin = {
+      enable = false;
+
+      settings = {
+        flavour = "mocha";
+        transparent_background = false;
+        term_colors = true;
+        default_integrations = true;
+        integrations = {
+          cmp = true;
+          neotree = true;
+          telescope.enabled = true;
+          dashboard = true;
+        };
+        background.dark = "mocha";
       };
-      background.dark = "mocha";
+    };
+    tokyonight = {
+      enable = true;
+
+      settings = {
+        plugins = {
+          all = true;
+          auto = true;
+        };
+        style = "night";
+        styles = {
+          comments.italic = true;
+          keywords.italic = false;
+        };
+        terminal_colors = true;
+        transparent = false;
+      };
     };
   };
 }

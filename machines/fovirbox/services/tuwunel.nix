17a8557a0a703cc7e8e93444fa99687067b387ad
Fovir <fovir@disroot.org>
2026-03-30 02:42:52 +0800

    feat: limit the resource usage of `tuwunel`

--- machines/fovirbox/services/tuwunel.nix
+++ machines/fovirbox/services/tuwunel.nix
@@ -3,6 +3,11 @@
     image = "ghcr.io/matrix-construct/tuwunel:latest";
 
     environment = import ../values/tuwunel-env.nix;
+    extraOptions = [
+      "--cpus=0.5"
+      "--memory=512m"
+      "--memory-swap=512m"
+    ];
     ports = ["127.0.0.1:2217:8008"];
     pull = "newer";
     volumes = ["${import ../values/podman-data-dir.nix}/tuwunel:/var/lib/tuwunel"];

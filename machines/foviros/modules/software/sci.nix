369b2b97f344bca0513d0467b7ca8303bc409a67
Fovir <fovir@disroot.org>
2026-04-10 22:18:37 +0800

    feat: add window rule

--- machines/foviros/modules/software/sci.nix
+++ machines/foviros/modules/software/sci.nix
@@ -3,10 +3,26 @@
 in {
   programs = import ../../values/sci.nix;
 
-  home-manager.users.${username}.wayland.windowManager.hyprland.settings = {
-    exec-once = [sciName];
-    windowrule = [
-      "workspace special, match:class .*${sciName}.*"
-    ];
+  home-manager.users.${username}.wayland.windowManager = {
+    hyprland.settings = {
+      exec-once = [sciName];
+      windowrule = [
+        "workspace special, match:class .*${sciName}.*"
+      ];
+    };
+    sway.config = {
+      window.commands = [
+        {
+          command = "move scratchpad";
+          criteria = {class = sciName;};
+        }
+      ];
+      startup = [
+        {
+          command = sciName;
+          always = true;
+        }
+      ];
+    };
   };
 }

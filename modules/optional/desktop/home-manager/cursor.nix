2dccc6d1e677a56fad5ea4ddf1c4a80a9ee1d6b8
Fovir <fovir@disroot.org>
2026-04-10 22:37:16 +0800

    refactor: extract `cursor` configuration to a single file

---
+++ modules/optional/desktop/home-manager/cursor.nix
@@ -0,0 +1,20 @@
+{username}: {pkgs, ...}: {
+  home-manager.users.${username} = {
+    home = {
+      pointerCursor = {
+        hyprcursor = {
+          enable = true;
+          size = 24;
+        };
+        name = "Bibata-Modern-Ice";
+        package = pkgs.bibata-cursors;
+        size = 24;
+        gtk.enable = true;
+        x11 = {
+          enable = true;
+          defaultCursor = "Bibata-Modern-Ice";
+        };
+      };
+    };
+  };
+}
--- modules/optional/desktop/hyprland/default.nix
+++ modules/optional/desktop/hyprland/default.nix
@@ -10,6 +10,7 @@
 
   imports = [
     (import ../../fcitx5.nix {inherit username;})
+    (import ../home-manager/cursor.nix {inherit username;})
     (import ../home-manager/fuzzel.nix {inherit username;})
     (import ../home-manager/gtk.nix {inherit username;})
     (import ../home-manager/hardwares.nix {inherit username;})
@@ -27,23 +28,6 @@
       ./hyprshell
       ./waybar
     ];
-
-    home = {
-      pointerCursor = {
-        hyprcursor = {
-          enable = true;
-          size = 24;
-        };
-        name = "Bibata-Modern-Ice";
-        package = pkgs.bibata-cursors;
-        size = 24;
-        gtk.enable = true;
-        x11 = {
-          enable = true;
-          defaultCursor = "Bibata-Modern-Ice";
-        };
-      };
-    };
     services.hyprpolkitagent.enable = true;
     xdg.portal = {
       enable = true;

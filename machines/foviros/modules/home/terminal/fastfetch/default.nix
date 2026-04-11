6920ddab270eb132b221c5eb0c2bcc8b9de0a647
Fovir <fovir@disroot.org>
2026-04-11 15:39:01 +0800

    refactor: refactor the `fastfetch` display

--- machines/foviros/modules/home/terminal/fastfetch/default.nix
+++ machines/foviros/modules/home/terminal/fastfetch/default.nix
@@ -1,8 +1,5 @@
 {...}: {
   programs.fastfetch.enable = true;
 
-  xdg.configFile = {
-    "fastfetch/config.jsonc".source = ./fastfetch.jsonc;
-    "fastfetch/logo.txt".source = ./logo.txt;
-  };
+  xdg.configFile. "fastfetch/config.jsonc".source = ./fastfetch.jsonc;
 }

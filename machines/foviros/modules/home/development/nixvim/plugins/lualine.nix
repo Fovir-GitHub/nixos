b311e5c00c3b0ed53e77e804416018861488567c
Fovir <fovir@disroot.org>
2026-04-19 14:22:04 +0800

    feat: show command in statusline

--- machines/foviros/modules/home/development/nixvim/plugins/lualine.nix
+++ machines/foviros/modules/home/development/nixvim/plugins/lualine.nix
@@ -2,6 +2,25 @@
   plugins.lualine = {
     enable = true;
 
-    settings.options.globalstatus = true;
+    settings = {
+      options.globalstatus = true;
+      sections = {
+        lualine_x = [
+          {
+            __unkeyed-1.__raw = ''
+              require("noice").api.status.command.get
+            '';
+            cond.__raw = ''
+              require("noice").api.status.command.has
+            '';
+            color = {fg = "#ff9e64";};
+          }
+
+          "encoding"
+          "fileformat"
+          "filetype"
+        ];
+      };
+    };
   };
 }

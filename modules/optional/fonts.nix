771a86ca7ec334efbfac0c51ef11a6e97d10c69c
Fovir <fovir@disroot.org>
2026-05-01 17:04:06 +0800

    feat: configure fonts

--- modules/optional/fonts.nix
+++ modules/optional/fonts.nix
@@ -4,12 +4,15 @@
     packages = with pkgs; [
       lxgw-wenkai
       nerd-fonts.jetbrains-mono
+      noto-fonts-color-emoji
+      sarasa-gothic
     ];
 
     fontconfig.defaultFonts = {
+      emoji = ["Noto Color Emoji"];
       monospace = ["JetBrainsMono Nerd Font Mono" "LXGW WenKai Mono"];
-      sansSerif = ["JetBrainsMono Nerd Font Mono" "LXGW WenKai Mono"];
-      serif = ["JetBrainsMono Nerd Font Mono" "LXGW WenKai Mono"];
+      sansSerif = ["Sarasa Gothic SC"];
+      serif = ["Sarasa Gothic SC"];
     };
   };
 }

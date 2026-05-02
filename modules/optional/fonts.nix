64097d9086db61dd0fb32b6d1f084ff51a918b74
Fovir <fovir@disroot.org>
2026-05-02 23:00:18 +0800

    refactor: use noto fonts

--- modules/optional/fonts.nix
+++ modules/optional/fonts.nix
@@ -4,15 +4,15 @@
     packages = with pkgs; [
       lxgw-wenkai
       nerd-fonts.jetbrains-mono
+      noto-fonts
       noto-fonts-color-emoji
-      sarasa-gothic
     ];
 
     fontconfig.defaultFonts = {
       emoji = ["Noto Color Emoji"];
       monospace = ["JetBrainsMono Nerd Font Mono" "LXGW WenKai Mono"];
-      sansSerif = ["Sarasa Gothic SC"];
-      serif = ["Sarasa Gothic SC"];
+      sansSerif = ["Noto Sans" "LXGW WenKai"];
+      serif = ["Noto Serif" "LXGW WenKai"];
     };
   };
 }

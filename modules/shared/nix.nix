63268ab93b4fdd69440088eef44f588383ba8620
Fovir <fovir@disroot.org>
2026-05-17 01:40:04 +0800

    feat: add `nixos-cuda` cache server

--- modules/shared/nix.nix
+++ modules/shared/nix.nix
@@ -12,10 +12,12 @@
     };
     settings = {
       substituters = [
+        "https://cache.nixos-cuda.org" # See https://nixos.wiki/wiki/CUDA
         "https://cache.nixos.org" # See https://nixos.wiki/wiki/Binary_Cache
         "https://nix-community.cachix.org" # See https://nix-community.org/cache/
       ];
       trusted-public-keys = [
+        "cache.nixos-cuda.org:74DUi4Ye579gUqzH4ziL9IyiJBlDpMRn9MBN8oNan9M="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];

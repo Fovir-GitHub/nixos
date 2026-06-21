991cbe1b4f372d643859cb70b5c4a3885eaee959
Fovir <fovir@disroot.org>
2026-06-22 02:53:15 +0800

    refactor: simplify the global `.editorconfig`
    Signed-off-by: Fovir <fovir@disroot.org>

--- machines/foviros/modules/home/development/formatters.nix
+++ machines/foviros/modules/home/development/formatters.nix
@@ -21,15 +21,9 @@
       root = true
 
       [*]
-      indent_style = space
-      indent_size = 2
-      trim_trailing_whitespace = true
       insert_final_newline = true
       end_of_line = lf
       charset = utf-8
-
-      [*.{c,cpp,h,hpp,go,rs,py}]
-      indent_size = 4
     '';
   };
 }
